8e2a7707d702709adb34de33720ba3855c35e5d3
Kumar, Pratyush (CT RDA DS AA DF PLT FH) <pratyush.kumar@siemens.com>
2023-04-27 01:13:08 +0530

    end of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null); //User | null --> UnionType
+  currentUser$ = this.currentUserSource.asObservable();
+  //$ signifies its an observable
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  //.pipe gives us access to RxJs operators so that we can transform or do something with that observable before a component subscribes to it.
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,30 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,  // provides 2-way binding between user input in html and the component .ts code
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()      
   ],
-  providers: [],
+  providers: [], //in earlier version of angular, when we created a service we needed to add the service here in providers[] 
+                 //but now it happens automatically with the providedIn metadata in service class 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
